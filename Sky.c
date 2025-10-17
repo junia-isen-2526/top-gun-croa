7f1250711c1f23859d874f83911b1dab21af35b7
kheldae <taho.gougeon@gmail.com>
2025-10-17 09:35:46 +0200

    empty sky

--- Sky.c
+++ Sky.c
@@ -3,3 +3,46 @@
 //
 
 #include "Sky.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Sky *createSky(int height, int width) {
+  Sky *sky = malloc(sizeof(Sky));
+  sky->height = height;
+  sky->width = width;
+  return sky;
+}
+
+void destroySky(Sky *sky) {
+  if (!sky)
+    return;
+  free(sky);
+}
+
+void displaySky(Sky *sky) {
+  if (!sky)
+    return;
+  for (unsigned int i = 0; i < sky->height; i++) {
+    for (unsigned int j = 0; j < sky->width; j++)
+      putchar(EMPTY_SKY_TILE);
+    putchar('\n');
+  }
+}
+
+int getSkyHeight(Sky *sky) {
+  if (!sky)
+    return -1;
+  return sky->height;
+}
+
+int getSkyWidth(Sky *sky) {
+  if (!sky)
+    return -1;
+  return sky->width;
+}
+
+void putMobileOnSky(Sky *sky, Mobile *mobile) {
+  if (!sky)
+    return;
+}
